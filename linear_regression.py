5152293e1a010408e2dff9a7fd7bc781f8f733fb
Patrick Bucher <patrick.bucher@mailbox.org>
2020-12-28 23:22:49 +0100

    more features

--- linear_regression.py
+++ linear_regression.py
@@ -3,13 +3,24 @@
 import numpy as np
 import pandas as pd
 
+
+def augment(df):
+    df['MSZoning'] = df['MSZoning'].map({'A': 0, 'C': 0, 'FV': 0, 'I': -10, 'RH': 10, 'RL': 50, 'RP': 100, 'RM': 25}).fillna(0)
+    exterior = {'Ex': 4, 'Gd': 3, 'TA': 2, 'Fa': 1, 'Po': 0}
+    df['ExterQual'] = df['ExterQual'].map(exterior).fillna(0)
+    df['ExterCond'] = df['ExterCond'].map(exterior).fillna(0)
+    df['BsmtQual'] = df['BsmtQual'].map({'Ex': 5, 'Gd': 4, 'TA': 3, 'Fa': 2, 'Po': 1, 'NA': 0}).fillna(0)
+    df['CentralAir'] = df['CentralAir'].map({'N': 0, 'Y': 1}).fillna(0)
+
 def normalize(arr):
     return (arr - arr.mean()) / arr.std()
+
 
 def add_polynomials(df, columns, max_grade=5):
     for col in columns:
         for p in range(2,max_grade+1):
             df[f'{col}{p}'] = df[col] ** p
+
 
 data = pd.read_csv('data/train.csv')
 input_cols = [
@@ -19,8 +30,14 @@ input_cols = [
     'OverallCond',
     'YearBuilt',
     'GrLivArea',
+    'MSZoning',
+    'ExterQual',
+    'ExterCond',
+    'BsmtQual',
+    'CentralAir',
 ]
 data = data.loc[:,input_cols+['SalePrice']].dropna()
+augment(data)
 
 Y = data.loc[:,'SalePrice'].to_numpy()
 data.drop(['SalePrice'], axis=1, inplace=True)
@@ -30,20 +47,21 @@ add_polynomials(data, input_cols)
 X_norm = normalize(data).to_numpy()
 
 theta = np.random.rand(X_norm.shape[1])
-alpha = 1e-3
+alpha = 1e-1
 lmbda = 1e-6
 m = len(X_norm)
 print(f'training with {m} examples')
-iterations = int(1e5)
+iterations = int(1e6)
+batch = iterations / 10
+
 for i in range(iterations):
     pred = X_norm.dot(theta)
     diff = pred - Y
     grad = diff.dot(X_norm)
-    theta -= (alpha/m) * grad
-    theta += (lmbda/m) * theta.sum()
+    theta -= (alpha/m) * (grad + (lmbda/m) * theta.sum())
     cost = 1/(2*m) * (diff ** 2).sum() + (lmbda/(2*m)) * (theta ** 2).sum()
-    if (i % (iterations / 10)) == 0:
-        print(f'cost: {cost:.5f}')
+    if i % batch == batch-1:
+        print(f'cost after {i} iterations: {cost:.5f}')
 
 print(f'weights: {theta}')
 
@@ -51,6 +69,7 @@ test_data = pd.read_csv('data/test.csv')
 id_col = test_data.loc[:,['Id']].to_numpy()
 test_data = test_data.loc[:,input_cols]
 
+augment(test_data)
 add_polynomials(test_data, input_cols)
 
 X_norm = normalize(test_data.fillna(0)).to_numpy()
